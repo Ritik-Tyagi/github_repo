67be14ea293e035a73135fbf04341b130bae5ca7
ritik-tyagi <ritiktyag6@gmail.com>
2024-01-11 08:34:04 +0530

    add QrCode creater

---
+++ QrCode.py
@@ -0,0 +1,12 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1, # type: ignore
+                 error_correction=qrcode.constants.ERROR_CORRECT_H, # type: ignore
+                 box_size=10,border=4,)
+
+qr.add_data("hello guyes")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="white")
+img.save("qrcode.png")
+
